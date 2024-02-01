7c9a7c6f828844d281631d7dcbf437cdb0430358
Nimisha Suresh <nimisha.suresh@aspiresys.com>
2024-02-01 10:39:15 +0530

    Firstcommit

---
+++ TASK 2_Disposable_GarbageCollector/Program.cs
@@ -0,0 +1,24 @@
+﻿class testClass : IDisposable
+{
+    public void Dispose()
+    {
+        // Dispose objects here
+ 	// clean resources
+ 	Console.WriteLine("Dispose Method!!!!");
+    }
+}
+
+//call class
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Use using statement with class that implements Dispose.
+ 	using (testClass objClass = new testClass())
+ 	{
+     		Console.WriteLine("Using Statment to execute the Dispose Method !!!!!!");
+	}
+	 Console.WriteLine("After Dispose method cleans up!!!!!!");
+    }
+}
+
